88ab92fb2b725ef6e92047f833a38322c35b3e4f
backink <r09921093@g.ntu.edu.tw>
2023-11-01 21:13:31 +0800

    Fix database version and exportSchema attribute

--- app/src/main/java/com/backink/beetlerecord/AppDatabase.java
+++ app/src/main/java/com/backink/beetlerecord/AppDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Project.class}, version = 0)
+@Database(entities = {Project.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
     public abstract ProjectDao projectDao();
